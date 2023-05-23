ffd8a96166f1df600b66ede84c6a4011f5d9a363
Kenny Weiss <kenny1weiss@aol.com>
2023-05-23 11:15:32 -0700

    first commit

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kennethweiss/Documents/CodingDojo/Python/django/my_environments/djangoenv/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
